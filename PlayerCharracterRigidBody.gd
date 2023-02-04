62a5c52a819b160015a37e59f38c90d0102f9561
Christopher <c.grassi@hotmail.fr>
2023-02-04 20:34:31 +0100

    fix jump

--- PlayerCharracterRigidBody.gd
+++ PlayerCharracterRigidBody.gd
@@ -7,6 +7,7 @@ export (float) var JUMP_STRENGTH = 12000
 export (float) var TRAMPOLINE_STRENGTH = 5000
 export var colliders = []
 
+var jumpPressed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,28 +15,31 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):dddzzd
-#	passddddddddddddddz
+#func _process(delta):
+
 
 
 func _physics_process(delta):
 	#Init
 	var velocity = Vector2.ZERO
-	
+
 	# Lateral movement
 	if Input.is_action_pressed("moveLeft"):
 		velocity.x -= WALK_SPEED
 	if Input.is_action_pressed("moveRight"):
 		velocity.x += WALK_SPEED
 	
-	#Jump
-	if Input.is_action_pressed("jump") and linear_velocity.y == 0:
-		velocity.y = -JUMP_STRENGTH
-
-#	Trampoline
 	colliders = get_colliding_bodies()
-	if colliders.size() > 0 && colliders[0] is Trampoline:
-		velocity.y = -TRAMPOLINE_STRENGTH
+	if colliders.size() > 0:
+		#Jump
+		if jumpPressed != Input.is_action_pressed("jump") and Input.is_action_pressed("jump"):
+			velocity.y = -JUMP_STRENGTH
+		#	Trampoline
+		if colliders[0] is Trampoline:
+			velocity.y = -TRAMPOLINE_STRENGTH
+
+	#Input
+	jumpPressed = Input.is_action_pressed("jump")
 
 	#Forces applying
 	apply_central_impulse(velocity * delta)
