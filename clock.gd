91ed42e2896896d7b582f500520413498f26d1f9
Christopher <c.grassi@hotmail.fr>
2023-02-04 15:01:08 +0100

    Integration wheel

---
+++ clock.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _draw():
+	draw_circle(Vector2.ZERO, 10, Color.orangered)
