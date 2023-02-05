8325f520aaddc9fd08c9e05ce6d9a872de0b1e5c
Estelle Rostan <estellerostan@pm.me>
2023-02-05 17:18:58 +0100

    Add a cinematic camera for the ending
    Thanks to @crazyrems :)

---
+++ PlayerCamera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+export (NodePath) var end_camera
+
+func _on_end_cinematic_trigger():
+	var end_camera_node: Camera2D = get_node(end_camera)
+	var _cur_pos = global_position
+	get_parent().remove_child(self)
+	end_camera_node.get_parent().add_child(self)
+	$Tween.interpolate_property(self, "global_position", _cur_pos, 
+	end_camera_node.global_position, 4, Tween.TRANS_EXPO, Tween.EASE_OUT)
+	$Tween.start()
+
+
+func _on_Area2D_body_entered(body):
+	_on_end_cinematic_trigger()
