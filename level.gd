e907295fba4abc0b0e53f09088c4aa82d4a7b04c
crazyrems <tauziac0remy+fork@gmail.com>
2023-02-12 17:45:01 +0100

    🎞 Add intro and ending screen

--- level.gd
+++ level.gd
@@ -3,3 +3,7 @@ extends Node2D
 func _process(delta):
 	if Input.is_action_just_pressed("debug_restart"):
 		get_tree().reload_current_scene()
+
+
+func _go_to_end_screen():
+	get_tree().change_scene_to(preload("res://ending.tscn"))
